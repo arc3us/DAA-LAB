3287266055a835e9f81a9510c1442b1c9bfda99c
arc3us <arinjayjain380@gmail.com>
2021-11-27 15:55:07 +0530

    Fractional Knapscak

---
+++ Lab 5/fractionalKnsapsack.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+void main()
+{
+    int capacity, no_items, cur_weight, item;
+    int used[10];
+    float total_profit;
+    int i;
+    int weight[10];
+    int value[10];
+
+    printf("Enter the capacity of knapsack:\n");
+    scanf("%d", &capacity);
+
+    printf("Enter the number of items:\n");
+    scanf("%d", &no_items);
+
+    printf("Enter the weight and value of %d item:\n", no_items);
+    for (i = 0; i < no_items; i++)
+    {
+        printf("Weight of object %d \t", i);
+        scanf("%d", &weight[i]);
+        printf("Value of object %d \t", i);
+        scanf("%d", &value[i]);
+    }
+
+    for (i = 0; i < no_items; ++i)
+        used[i] = 0;
+
+    cur_weight = capacity;
+    while (cur_weight > 0)
+    {
+        item = -1;
+        for (i = 0; i < no_items; ++i)
+            if ((used[i] == 0) &&
+                ((item == -1) || ((float) value[i] / weight[i] > (float) value[item] / weight[item])))
+                item = i;
+
+        used[item] = 1;
+        cur_weight -= weight[item];
+        total_profit += value[item];
+        if(cur_weight < 0)
+        {
+            total_profit -= value[item];
+            total_profit += (1 + (float)cur_weight / weight[item]) * value[item];
+        }
+    }
+
+    printf("Filled the bag with objects valued %.2f \n", total_profit);
+}
