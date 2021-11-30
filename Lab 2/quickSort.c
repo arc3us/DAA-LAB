8b0657aa3a106865952ca391d2e49347d4c6721a
arc3us <arinjayjain380@gmail.com>
2021-11-30 23:06:17 +0530

    QuickSort with first element as Pivot

--- Lab 2/quickSort.c
+++ Lab 2/quickSort.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-#define MAX 100
 
-//function to print the array
-void printArray(int arr[], int size)
+
+void arrayPrint(int arr[], int size)
 {
     int i;
     for (i = 0; i < size; i++)
@@ -12,63 +11,50 @@ void printArray(int arr[], int size)
     printf("\n");
 }
 
-int partition(int arr[], int low, int high)
-{
-    int pivot = arr[high]; // pivot
-    int i = (low - 1);     // Index of smaller element and indicates the right position of pivot found so far
+int partition(int *a, int start, int end)
 
-    for (int j = low; j < high; j++)
+{
+
+    int pivot = a[start], p1 = start + 1, i, temp;
+
+    for (i = start + 1; i <= end; i++)
     {
-        // If current element is smaller than the pivot
-        if (arr[j] < pivot)
+
+        if (a[i] < pivot)
         {
-            i++; // increment index of smaller element
-            // swap
-            int t = arr[i];
-            arr[i] = arr[j];
-            arr[j] = t;
+            if (i != p1)
+            {
+                temp = a[p1];
+                a[p1] = a[i];
+                a[i] = temp;
+            }
+            p1++;
         }
     }
-    int t = arr[i + 1];
-    arr[i + 1] = arr[high];
-    arr[high] = t;
-    return (i + 1);
+
+    a[start] = a[p1 - 1];
+    a[p1 - 1] = pivot;
+
+    return p1 - 1;
 }
 
-/* The main function that implements QuickSort 
-arr[] --> Array to be sorted, 
-low --> Starting index, 
-high --> Ending index */
-void quickSort(int arr[], int low, int high)
+void quicksort(int *a, int start, int end)
 {
-    if (low < high)
+    int p1;
+    if (start < end)
     {
-        /* partIndex is partitioning index, arr[p] is now 
-        at right place */
-        int partIndex = partition(arr, low, high);
-
-        // Separately sort elements before
-        // partition and after partition
-        quickSort(arr, low, partIndex - 1);
-        quickSort(arr, partIndex + 1, high);
+        p1 = partition(a, start, end);
+        quicksort(a, start, p1 - 1);
+        quicksort(a, p1 + 1, end);
     }
 }
 
-int main()
+void main()
 {
-    int size;
-    printf("\nEnter array size: ");
-    scanf("%d", &size);
-    printf("\nEnter values: ");
-    int arr[MAX];
-    int i;
-    for (i = 0; i < size; i++)
-    {
-        scanf("%d", &arr[i]);
-    }
-    printf("Unsorted aray: \n");
-    printArray(arr, size);
-    quickSort(arr, 0, size - 1);
-    printf("Sorted aray: \n");
-    printArray(arr, size);
+    int arr[] = {3, 2, 5, 1};
+
+    arrayPrint(arr, 4);
+    quicksort(arr,0,4);
+    arrayPrint(arr,4);
+
 }
